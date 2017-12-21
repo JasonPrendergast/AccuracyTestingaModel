40cd4853cb114d2a27bd979d141870d4a4ca633c
Jason Prendergast <32454226+JasonPrendergast@users.noreply.github.com>
2017-12-21 22:59:07 +0000

    Add files via upload

---
+++ testAcc2_threads.py
@@ -0,0 +1,285 @@
+import tensorflow as tf
+import pickle
+import numpy as np
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import WordNetLemmatizer
+from bottle import Bottle, ServerAdapter, route, run, request, template
+import re
+import math
+import itertools
+import sys
+import html as htmla
+import threading, time, random
+import queue as Queue
+
+train_x,train_y,test_x,test_y = pickle.load(open("jobs_set_10000_0.pickle","rb"))
+qfeatures=Queue.Queue(900)
+qlabels=Queue.Queue(900)
+finishedflag=0
+outputarray=[]
+errorcount=0
+
+#####################################################################
+#####################################################################
+lemmatizer = WordNetLemmatizer()
+
+n_nodes_hl1 = 1000
+#print(type(n_nodes_hl1))
+n_nodes_hl2 = 500
+n_nodes_hl3 = 1000
+
+n_classes = 64029
+#hm_data = 2000000
+with open('lexicon_jt_64029.pickle','rb') as f:
+    print('label')
+    labellexicon = pickle.load(f)
+    print(len(labellexicon))
+with open('lexicon_jd_22694.pickle','rb') as f:
+    print('features')
+    featurelexicon = pickle.load(f)
+    print(len(featurelexicon))
+
+#batch_size = 128
+#hm_epochs = 10
+#tf.reset_default_graph()
+x = tf.placeholder('float',shape=(None, 22694))
+y = tf.placeholder('float',shape=(None, 64029))
+
+hidden_1_layer = {'f_fum':n_nodes_hl1,
+                  'weight':tf.Variable(tf.random_normal([22694, n_nodes_hl1])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+
+hidden_2_layer = {'f_fum':n_nodes_hl2,
+                  'weight':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+hidden_3_layer = {'f_fum':n_nodes_hl3,
+                  'weight':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+output_layer = {'f_fum':None,
+                'weight':tf.Variable(tf.random_normal([n_nodes_hl2, n_classes])),
+                'bias':tf.Variable(tf.random_normal([n_classes])),}
+
+#####################################################################
+#                       DEFINE NETWORK HERE                         #
+#####################################################################
+def neural_network_model(data):
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weight']), hidden_1_layer['bias'])
+    l1 = tf.nn.relu(l1)
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weight']), hidden_2_layer['bias'])
+    l2 = tf.nn.tanh(l2)#relu(l2)
+    l3 = tf.add(tf.matmul(l1,hidden_2_layer['weight']), hidden_2_layer['bias'])
+    l3 = tf.nn.tanh(l3)
+    output = tf.matmul(l3,output_layer['weight']) + output_layer['bias']
+    #print(output)
+    return output
+
+#tf.reset_default_graph()
+#saver = tf.train.Saver()
+#tf.reset_default_graph()
+saver = tf.train.import_meta_graph('./modelMiddle.ckpt.meta')
+#
+#
+#
+class Producer:
+    def __init__(self,test_x,train_y,featurelexicon,labellexicon):
+        self.test_x=test_x
+        self.test_y=test_y
+        self.nextTime=0
+        self.i=0
+        self.featurelexicon=featurelexicon
+        self.labellexicon=labellexicon
+        self.end=False
+        
+    def __del__(self):
+        print ("deleted")
+        
+    def run(self):
+        global qfeatures,qlabels,errorcount
+        while (self.i < len(self.test_x)):
+            if (self.i/100).is_integer():
+                print('inserting: '+str(self.i))
+            batch_x = []
+            batch_y = []
+            
+            if(self.nextTime<time.clock()):
+                jobDescription = self.test_x[self.i]
+                
+                            
+                features = np.zeros(len(self.featurelexicon))
+
+                jobDescription =jobDescription.strip().split()
+                
+                
+
+                for word in jobDescription:
+                    
+                    if word.lower() in self.featurelexicon:
+                       
+                        index_value = self.featurelexicon.index(word.lower())
+                        features[index_value] += 1
+                line_x = list(features)
+                #----------------------------------------------------
+                #                        y
+                #----------------------------------------------------
+
+                jobtitle = self.test_y[self.i]
+                            
+                label = np.zeros(len(self.labellexicon))                        
+        
+                fline=jobtitle.strip()                
+
+                
+                if fline.lower() in self.labellexicon:
+                                               
+                    index_value = self.labellexicon.index(fline.lower())
+                    
+                    label[index_value] += 1
+                    line_y = list(label)
+                    
+                            
+                            
+                else:
+                    line_y = 'nan'
+                    print(jobtitle)
+                    print('y nan')
+
+                if all(v== 0 for v in line_x):
+                    print('error x')
+
+                        
+                            
+                #Remove any array that is full of zero or set to string nan          
+                if not all(v== 0 for v in line_x) and not line_y == 'nan':
+                            
+                    
+                    qfeatures.put(line_x)
+                    qlabels.put(line_y)
+                    self.nextTime+=(random.random())/1000
+                    self.i+=1
+                else:
+                    
+                    self.nextTime+=(random.random())/1000
+                    self.i+=1
+                    errorcount+=1
+    
+            fline=''
+            jobtitle=''
+            current_words=[]
+        
+
+
+
+class Consumer:
+    def __init__(self,maximum,sess,prediction):
+        self.nextTime=1
+        self.max=(maximum)
+        self.i=0
+        self.sess=sess
+        self.prediction=prediction
+        #self.optimizer=optimizer
+       # self.cost=cost
+
+    def __del__(self):
+        print ("deleted")
+        
+    def run(self):
+        
+        global qfeatures,finishedflag,qlabels,errorcount
+        batch_x=[]
+        batch_y = []
+        
+        while (self.i<(self.max-(errorcount))):
+           if(self.nextTime<time.clock() and not qfeatures.empty()):
+                batch_x.append(qfeatures.get())
+                batch_y.append(qlabels.get())
+
+                result = (self.sess.run(tf.argmax(self.prediction.eval(feed_dict={x:[np.array(batch_x)]}),1)))
+
+                batch_x = []
+                batch_y = []
+                self.nextTime+=(random.random()*2)/1000
+                self.i+=1
+                outputarray.apppend(str((labellexicon[int(result)])))
+                
+                
+                
+        finishedflag=1
+               
+#####################################################################
+#                       USE NETWORK HERE                            #
+#####################################################################
+def use_neural_network():
+    global finishedflag
+    global train_x,train_y,test_x,test_y
+    global outputarray
+    global errorcount
+    hit=0
+    count=0
+    
+    prediction = neural_network_model(x)
+   
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        #jobDescription = input_data
+        p=Producer(test_x,test_y,featurelexicon,labellexicon)
+        c=Consumer(len(test_x),sess,prediction)
+        pt=threading.Thread(target=p.run,args=())
+        ct=threading.Thread(target=c.run,args=())
+        pt.start()
+        time.sleep(30)   
+        ct.start()
+
+        while (finishedflag==0):
+            time.sleep(20)
+        print('ending')
+        print(finishedflag)
+
+        for i in range(1000):
+            print(test_y[i])
+            print(outputarray[i])
+    
+        if test_y[i] == outputarray[i]:
+            
+            hit+=1
+    total = len(test_y)
+
+    print(hit)
+    print(total)
+
+use_neural_network()
+        
+    #return str((labellexicon[int(result)]))
+
+
+
+#print(str(use_neural_network('sales highstreet retail manager')))
+def test_network():
+    global outputarray
+    hit=0
+    count=0
+    #for t in test_x:
+    for i in range(1000):
+        count+=1
+        print(count)
+        outputarray.append(use_neural_network(test_x[i]))
+
+    #for i in range(len(test_y)):
+    for i in range(1000):#range(len(test_y)):
+        print(test_y[i])
+        print(outputarray[i])
+    
+        if test_y[i] == outputarray[i]:
+            
+            hit+=1
+    total = len(test_y)
+
+    print(hit)
+    print(total)
+
+#test_network()
+
+
+
